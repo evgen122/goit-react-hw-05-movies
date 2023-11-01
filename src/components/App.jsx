66c2e0190f4654401399605ceed214f01c41b04e
evgen122 <evg.khristenko@gmail.com>
2023-11-01 15:13:30 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,19 @@
+import { Route, Routes, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from 'path/to/pages/Home';
+import About from 'path/to/pages/About';
+import Products from 'path/to/pages/Products';
+import NotFound from 'path/to/pages/NotFound';
+import ProductDetails from 'path/to/pages/ProductDetails';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
     <div
@@ -7,10 +23,26 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* React homework template */}
+
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
