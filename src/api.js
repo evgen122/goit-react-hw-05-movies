caeae2ab744ea49b2c7686c1ce2a42c6c806a829
evgen122 <evg.khristenko@gmail.com>
2023-11-02 16:58:21 +0200

    1

--- src/api.js
+++ src/api.js
@@ -1,1 +1,37 @@
+import axios from 'axios';
 // 9ad4d94cf88f6011a6f91c12beaa36d6
+
+const key = 'api_key=9ad4d94cf88f6011a6f91c12beaa36d6';
+
+axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+
+export const fetchTrending = async () => {
+  const response = await axios.get(`/trending/all/day?${key}`);
+  return response.data;
+};
+
+export const fetchSearchMovies = async query => {
+  const response = await axios.get(
+    `/search/movie?${key}&query=${query}&include_adult=false&language=en-US`
+  );
+  return response.data;
+};
+
+export const fetchMoviesDetails = async movieId => {
+  const response = await axios.get(`/movie/${movieId}?${key}&language=en-US`);
+  return response.data;
+};
+
+export const fetchMovieCredits = async movieId => {
+  const response = await axios.get(
+    `/movie/${movieId}/credits?${key}&language=en-US`
+  );
+  return response.data;
+};
+
+export const fetchMovieReviews = async movieId => {
+  const response = await axios.get(
+    `/movie/${movieId}/reviews?${key}&language=en-US`
+  );
+  return response.data;
+};
