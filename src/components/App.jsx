490734a5b5386321396fa6e90d94b99753bfa631
evgen122 <evg.khristenko@gmail.com>
2023-11-02 09:27:46 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,18 @@
-import { Route, Routes, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-import Home from 'path/to/pages/Home';
-import About from 'path/to/pages/About';
-import Products from 'path/to/pages/Products';
-import NotFound from 'path/to/pages/NotFound';
-import ProductDetails from 'path/to/pages/ProductDetails';
+// import { Route, Routes, NavLink } from 'react-router-dom';
+// import styled from 'styled-components';
+// import Home from 'path/to/pages/Home';
+// import About from 'path/to/pages/About';
+// import Products from 'path/to/pages/Products';
+// import NotFound from 'path/to/pages/NotFound';
+// import ProductDetails from 'path/to/pages/ProductDetails';
 
-const StyledLink = styled(NavLink)`
-  color: black;
+// const StyledLink = styled(NavLink)`
+//   color: black;
 
-  &.active {
-    color: orange;
-  }
-`;
+//   &.active {
+//     color: orange;
+//   }
+// `;
 
 export const App = () => {
   return (
@@ -28,21 +28,21 @@ export const App = () => {
     >
       {/* React homework template */}
 
-      <nav>
+      {/* <nav>
         <StyledLink to="/" end>
           Home
         </StyledLink>
         <StyledLink to="/about">About</StyledLink>
         <StyledLink to="/products">Products</StyledLink>
-      </nav>
+      </nav> */}
 
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:productId" element={<ProductDetails />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 };
