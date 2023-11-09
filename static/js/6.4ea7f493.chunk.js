1313d897a2b3941469dc994cea5916cb2bce8929
evgen122 <129151515+evgen122@users.noreply.github.com>
2023-11-09 14:05:31 +0000

    Deploying to gh-pages from @ evgen122/goit-react-hw-05-movies@f5a830f5c9471aa1a0791e89892859a1b442cffc 🚀

--- static/js/6.4ea7f493.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[6],{6:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("h2",{children:"NotFound"})}}}]);
-//# sourceMappingURL=6.4ea7f493.chunk.js.map
